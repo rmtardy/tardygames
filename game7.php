243788eb222b10b66971c8a800ead9e6c85b5aec
darwin <darwin@lampcamp.net>
2015-07-17 21:17:49 +0000

    july17

--- game7.php
+++ game7.php
@@ -1,9 +1,12 @@
 <?php
 require 'types.php';
-$typeCheck = true;
+$typeCheckA = true;
+$typeCheckA2 = true;
 $typeCheckD = true;
 $typeCheckD2 = true;
 $typeCheckM = true;
+$yesCheckA = true;
+$yesCheckD = true;
 $stab = 1;
 $type_array = array('NORMAL', 'FIRE', 'WATER', 'GRASS', 'ELECTRIC', 'ROCK',
                     'GROUND', 'ICE', 'FIGHTING', 'POISON', 'PSYCHIC', 'FLYING',
@@ -18,8 +21,8 @@ $level = 50;
 
 
 while($typeCheckM){
-    $typeA = strtoupper(trim(readline("What is the type of the attack move: ")));
-    if (!in_array($typeA, $type_array)){
+    $moveType = strtoupper(trim(readline("What is the type of the attack move: ")));
+    if (!in_array($moveType, $type_array)){
         printf ("That is not a valid type of attack move. ");
         }
     else {
@@ -37,39 +40,79 @@ while($typeCheckD){
         }
     }
 
-$multiType = strtoupper(trim(readline("Does the defender have a second type? Yes or No: ")));
-
-if($multiType == "YES"){
-    while($typeCheckD2){
-        $typeD2 = strtoupper(trim(readline("What is the second type of the defender: ")));
-        if (!in_array($typeD2, $type_array)){
-            printf("That is not a valid type of defender. ");
-            }
-        else {
-            $typeCheckD2 = false;
-            }
+while($yesCheckD){
+    $multiTypeD = strtoupper(trim(readline("Does the defender have a second type? Yes or No: ")));
+    if ($multiTypeD == "YES" || $multiTypeD == "NO"){
+        $yesCheckD = false;
         }
-        $type1 = getTypeMultiplier($typeA, $typeD1);
-        $type2 = getTypeMultiplier($typeA, $typeD2);
-        $type = $type1 * $type2;
     }
-    if($multiType == "NO") {
-            $type = getTypeMultiplier($typeA, $typeD1);
-        }
+        if($multiTypeD == "YES"){
+                while ($typeCheckD2){
+                    $typeD2 = strtoupper(trim(readline("What is the second type of the defender: ")));
+                    if (!in_array($typeD2, $type_array)){
+                        printf("That is not a valid type of defender. ");
+                    }
+                    else {
+                        $typeCheckD2 = false;
+                    }
+                }
+                    $type1 = getTypeMultiplier($moveType, $typeD1);
+                    $type2 = getTypeMultiplier($moveType, $typeD2);
+                    $type = $type1 * $type2;
+                }
+        if($multiTypeD == "NO") {
+            $type = getTypeMultiplier($moveType, $typeD1);
+            }
 
-while($typeCheck){
-    $moveType = strtoupper(trim(readline("What is the type of the attacker? ")));
-    if (!in_array($moveType, $type_array)){
+
+
+while ($typeCheckA){
+    $typeA = strtoupper(trim(readline("What is the first type of the attacker? ")));
+    if (!in_array($typeA, $type_array)){
         printf ("That is not a valid type of attacker. ");
         }
     else {
-        $typeCheck = false;
+        $typeCheckA = false;
         }
     }
 
-if($moveType == $typeA){
-    $stab = 1.5;
-    }
+while ($yesCheckA){
+    $multiTypeA = strtoupper(trim(readline("Does the attacker have a second type? Yes or No: ")));
+        if($multiTypeA == "YES" || $multiTypeA == "NO"){
+            $yesCheckA = false;
+        }
+        if ($multiTypeA == "YES"){
+            while ($typeCheckA2){
+                $typeA2 = strtoupper(trim(readline("What is the second type of the attacker: ")));
+                    if (!in_array($typeA2, $type_array)){
+                        printf("That is not a valid type of attacker. ");
+                        }
+                    else {
+                        $typeCheckA2 = false;
+                        }
+                    }
+                    if ($moveType == $typeA2 || $moveType == $typeA){
+                        $stab = 1.5;
+                        }
+            }
+            if ($multiTypeA == "NO"){
+                $yesCheckA = false;
+                if($typeA == $typeA){
+                       $stab = 1.5;
+                        }
+            }
+        // if ($multiTypeA !== "YES" || $multiTypeA !== "NO"){
+        //     printf("Check spelling. ");
+        //     }
+        }
+
+
+
+// if($multiTypeA == "NO") {
+//     if($typeA == $typeA){
+//         $stab = 1.5;
+//         }
+//     }
 
 echo $type ."\n";
 echo $stab ."\n";
